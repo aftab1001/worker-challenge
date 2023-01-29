de1580749a74ee6a5a2568a69de1a9662f056df8
Aftab Ur Rehman <aftab.ur.rehman@geomand.com>
2023-01-29 09:17:17 +0100

    added new package

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,31 @@
 import axios from 'axios';
-import { Worker, isMainThread } from 'worker_threads';
 
-const NUM_WORKERS = 8;
-const NUM_SAMPLES = 150;
-const REQUEST_URL = 'http://localhost:3001/rnd?n=1';
-const MAX_TIME = 10000; // 10 seconds
-
-let totalSum = 0;
-let totalTime = 0;
-let flakyWorkers = 0;
-
-async function workerThread(workerIndex: number) {
-    try {
-        const startTime = Date.now();
-        const response = await axios.get(REQUEST_URL);
-        const data = response.data.split('\n');
-
-        data.forEach((str: any) => {
-            if (str) {
-                const [, value] = str.split('=');
-                totalSum += Number(value);
-            }
-        });
-
-        const endTime = Date.now();
-        totalTime += endTime - startTime;
-    } catch (err: any) {
-        console.log(`Worker ${workerIndex} failed: ${err.message}`);
-        flakyWorkers++;
-    }
+async function collectSample(workerId: number) {
+    const response = await axios.get(`http://localhost:3001/rnd?n=${workerId}`);
+    const sample = response.data.rnd;
+    return sample;
 }
 
-if (isMainThread) {
-    for (let i = 0; i < NUM_WORKERS; i++) {
-        const worker = new Worker(__filename);
-        worker.on('message', (message) => {
-            console.log(`Worker ${i} completed: ${message}`);
-        });
-        worker.on('error', (err) => {
-            console.log(`Worker ${i} failed: ${err.message}`);
-        });
-        worker.on('exit', (code) => {
-            if (code !== 0) {
-                console.log(`Worker ${i} stopped with exit code ${code}`);
-            }
-        });
-    }
+async function scaleWorkerApp(numSamples: number, timeConstraint: number) {
+    const startTime = Date.now();
+    const i = 0;
+    const workerIds = [1, 2, 3]; // workerIds can be modified as per requirement
+    const samplePromises = workerIds.map(async (workerId) => {
+        const samples: number[] = [];
+        while (samples.length < numSamples / workerIds.length) {
+            const sample = await collectSample(workerId);
+            samples.push(sample);
+        }
+        return samples;
+    });
+    const allSamples = await Promise.all(samplePromises);
+    const flattenSamples = allSamples.flat();
+    const totalSum = flattenSamples.reduce((a, b) => a + b, 0);
+    const totalTime = (Date.now() - startTime) / 1000;
 
-    setTimeout(() => {
-        console.log(`Total sum: ${totalSum}`);
-        console.log(`Total time: ${totalTime}`);
-        console.log(`Flaky workers: ${flakyWorkers}`);
-    }, MAX_TIME);
-} else {
-    for (let i = 0; i < NUM_SAMPLES / NUM_WORKERS; i++) {
-        workerThread(i);
-    }
+    console.log(`Total Sum: ${totalSum}`);
+    console.log(`Total Time: ${totalTime} sec`);
+    console.log(`Sample Size: ${flattenSamples.length}`);
 }
+
+scaleWorkerApp(150, 10);
